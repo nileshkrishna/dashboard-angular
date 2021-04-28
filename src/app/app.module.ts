52147c749f717bd2bf92f3f09ab3cc5196218ee8
nileshkrishna <81105528+nileshkrishna@users.noreply.github.com>
2021-04-28 20:55:21 +0530

    Removed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ChartsModule } from 'ng2-charts';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,7 +14,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule
-  ],
+, ChartsModule  ],
   providers: [],
   bootstrap: [AppComponent]
 })
