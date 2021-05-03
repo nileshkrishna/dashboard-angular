736124431b525c2c8f95aa29893b707ab1160682
nileshkrishna <81105528+nileshkrishna@users.noreply.github.com>
2021-05-03 12:10:30 +0530

    Changed size of table

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, Input  } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
-  @Input() title: string;
+  @Input() title?: string;
 
   constructor() { }
 
